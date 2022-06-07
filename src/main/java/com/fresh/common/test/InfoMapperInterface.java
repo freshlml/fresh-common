09ec605ee659275c63ee7687f2ea3d1d0178b3d8
fresh <1326191992@qq.com>
2022-06-07 16:24:55 +0800

    1

--- src/main/java/com/fresh/common/test/InfoMapperInterface.java
+++
@@ -1,6 +0,0 @@
-package com.fresh.common.test;
-
-public interface InfoMapperInterface {
-    @HereSelect("select info_name from info")
-    String getInfoName(String param);
-}
--- src/main/java/com/fresh/common/test/UserMapperInterface.java
+++
@@ -1,6 +0,0 @@
-package com.fresh.common.test;
-
-public interface UserMapperInterface {
-    @HereSelect("select user_name from user")
-    String getUserName(String param);
-}
