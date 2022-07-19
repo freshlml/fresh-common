c53958e556d1c4d645fd032becdc2606151f1865
freshlml <1326191992@qq.com>
2022-07-19 15:55:08 +0800

    注解测试代码整理

--- src/main/java/com/fresh/common/utils/reflect/anno/DemoAnno.java
+++
@@ -1,9 +0,0 @@
-package com.fresh.common.utils.reflect.anno;
-
-import java.lang.annotation.*;
-
-@Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE,ElementType.PARAMETER})
-public @interface DemoAnno {
-}
--- src/main/java/com/fresh/common/utils/reflect/anno/ForTestAnnotatedElement.java
+++ src/main/java/com/fresh/common/utils/reflect/anno/ForTestAnnotatedElement.java
@@ -1,39 +1,13 @@
 package com.fresh.common.utils.reflect.anno;
 
 import java.lang.annotation.*;
-import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class ForTestAnnotatedElement {
 
     public static void main(String argv[]) {
-        testParameterAnnotations();
-        System.out.println("##########################");
+
         testAnnotatedElement();
-
-    }
-
-    @InAnno //not captured
-    private static class Base {}
-    private static class Bean extends Base {}
-    //private void m(@PoAnnotation(name="a") @PoAnnotation(name="b") Bean t) {} Container annotation '@PosAnnotation' is not applicable to parameter
-    private void m(@DemoAnno @PoAnnotation(name="a") Bean t) {}
-
-    private static void testParameterAnnotations() {
-        //java.lang.reflect.Executable#getParameterAnnotations
-        Method[] mds = ForTestAnnotatedElement.class.getDeclaredMethods();
-        Method method = null;
-        for(int i=0; i<mds.length; i++) {
-            if(mds[i].getName() == "m") {
-                method = mds[i];
-                break;
-            }
-        }
-        if(method != null) {
-            Annotation[][] pannos = method.getParameterAnnotations();
-
-            System.out.println(pannos);
-        }
     }
 
 
