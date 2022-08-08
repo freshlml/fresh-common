6633e19ebb71436dbd17c2485e8888b5f4eeb8fe
freshlml <1326191992@qq.com>
2022-08-08 18:37:15 +0800

    1

--- src/main/java/com/fresh/common/utils/StringUtils.java
+++ src/main/java/com/fresh/common/utils/StringUtils.java
@@ -11,46 +11,45 @@ public abstract class StringUtils {
     private static final String CLASS_SUFFIX = "class";
 
 
-    /*
+    /**
      * 将className 转化为 classpath
      * eg:
      *     com.sc.common.vo.JsonResult   ->  com/sc/common/vo/JsonResult.class
      *     com.sc.common.vo              ->  com/sc/common/vo
      * @param className 包名 或者 类的qualified name
-     * @param suffix
-     * @return a classpath,能够直接加载此classpath资源
-     *                  @see ClassLoader#getResource(String)
+     * @param suffix 是否添加.class后缀
+     * @return classpath for ClassLoader#getResource(String)
      */
     public static String className2classpath(String className, boolean suffix) {
         if(!hasLength(className)) return className;
+
         String classpath = className.replace(PACKAGE_SEP, PATH_SEP);
         if(suffix) classpath += PACKAGE_SEP + CLASS_SUFFIX;
         return classpath;
     }
 
-    /*
+    /**
      * 将classpath 转化为 className
      * eg:
      *      com/sc/common/vo/JsonResult.class  ->  com.sc.common.vo.JsonResult
      *      com/sc/common/vo                   ->  com.sc.common.vo
      * @param classpath classpath
-     * @return className,包名 或者 类的qualified name
+     * @return className包名 或者 类的qualified name
      */
     public static String classpath2ClassName(String classpath) {
         if(!hasLength(classpath)) return classpath;
+
         while(classpath.startsWith("/")) {
             classpath = classpath.substring(1);
         }
-        if(classpath.endsWith(PACKAGE_SEP+CLASS_SUFFIX)) {
-            classpath = classpath.substring(0, classpath.length()-(PACKAGE_SEP+CLASS_SUFFIX).length());
+        if(classpath.endsWith(PACKAGE_SEP + CLASS_SUFFIX)) {
+            classpath = classpath.substring(0, classpath.length()-(PACKAGE_SEP + CLASS_SUFFIX).length());
         }
         return classpath.replace(PATH_SEP, PACKAGE_SEP);
     }
 
     /*
      * 是否是16进制
-     * @param str
-     * @return
      */
     public static boolean isHexNumber(String str) {
         if(str == null) return false;
@@ -59,19 +58,16 @@ public abstract class StringUtils {
     }
 
 
-    /*
-     * 如果参数str不为null并且length>0,则返回true
-     * @param str
-     * @return
+    /**
+     * @param str string
+     * @return true of false
      */
     public static boolean hasLength(String str) {
-        return str!=null && str.length()>0;
+        return str != null && str.length() > 0;
     }
 
     /*
      * 去除所有whitespace
-     * @param str
-     * @return
      */
     public static String trimAllWhitespace(String str) {
         if (str == null || str.isEmpty()) {
