0ba028e6bc23992129dd982423da0ad8db6cfe3c
freshlml <1326191992@qq.com>
2024-08-10 11:22:57 +0800

    1

---
+++ src/main/java/com/fresh/common/utils/sql/IllFormedSqlException.java
@@ -0,0 +1,7 @@
+package com.fresh.common.utils.sql;
+
+public class IllFormedSqlException extends RuntimeException {
+    public IllFormedSqlException(String message) {
+        super(message);
+    }
+}
