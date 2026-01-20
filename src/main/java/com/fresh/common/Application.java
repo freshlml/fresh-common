fa58e021316552baaa100f6898d3cafa26eafc9e
freshlml <1326191992@qq.com>
2026-01-20 22:11:30 +0800

    1

--- src/main/java/com/fresh/common/Application.java
+++ src/main/java/com/fresh/common/Application.java
@@ -2,7 +2,7 @@ package com.fresh.common;
 
 public class Application {
 
-    public static void main(String argv[]) {
+    public static void main(String[] argv) {
         System.out.println("fresh-common start ... ");
     }
 }
