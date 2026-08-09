630ccf04286dad450257bea68deb8895016d8461
freshlml <1326191992@qq.com>
2026-08-09 19:26:20 +0800

    优化，修正

--- src/main/java/com/fresh/core/utils/NumberUnitUtils.java
+++ src/main/java/com/fresh/core/utils/NumberUnitUtils.java
@@ -4,7 +4,9 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.*;
 
-public abstract class NumberUnitUtils {
+public final class NumberUnitUtils {
+
+    private NumberUnitUtils() {}
 
     private static final Map<Integer, String> CONV = new HashMap<>();
     private static final List<Integer> defaultSkips = new ArrayList<>();
@@ -105,83 +107,54 @@ public abstract class NumberUnitUtils {
         return Double.valueOf(Math.pow(10, num)).longValue();
     }
 
-    /*
-     *Number类型之间相互转换
-     *继承Number的子类型(标准库)有: Byte,Short,Integer,Long,BigInteger,Float,Double,BigDecimal
+    /**
+     * <p>数值（Byte, Short, Integer, Long, Float, Double, BigInteger, BigDecimal）之间相互类型转换.</p>
      *
+     * <ul>
+     *   <li>目标类型是整数（Byte, Short, Integer, Long）
+     *      <ul>
+     *          <li>如果源类型是整数类型或 BigInteger，当数值未超界时，可以得到正确的数值；否则，抛异常</li>
+     *          <li>如果原类型是浮点类型或 BigDecimal，先将小数部分和小数点去掉得到整数部分，当数值未超界时，可以得到正确的数值；否则，抛异常</li>
+     *      </ul>
+     *   </li>
+     *   <li>目标类型是浮点数（Float, Double）
+     *       <ul>
+     *           <li>当数值超过浮点数的表示范围时，结果为 ∞</li>
+     *       </ul>
+     *   </li>
+     * </ul>
      *
-     *整数补码的截断与补值 (整数的强制类型转换，eg:byte i = (short) 128)
-     * 0:      0000 0000                0000 0000 | 0000 0000
-     * 1:      0000 0001                0000 0000 | 0000 0001
-     * 127:    0111 1111                0000 0000 | 0111 1111
-     *
-     * -1:     1111 1111                1111 1111 | 1111 1111
-     * -126:   1000 0010                1111 1111 | 1000 0010
-     * -127:   1000 0001                1111 1111 | 1000 0001
-     * -128: 1 1000 0000                1111 1111 | 1000 0000
-     *1.大 -> 小， 补码截断，当数值未超过"小类型"的边界时，可以得到正确的数值
-     *2.小 -> 大， 正数-补0;负数-补1，可以得到正确的数值
-     *
-     *
-     *number转换成整数(Byte,Short,Integer,Long)
-     * 1.number是整数，eg Long#intValue
-     *   当number数值未超过target的边界时，得到正确的数值。当number数值超过target的边界时，抛异常
-     *
-     * 2.BigInteger转换成整数: BigInteger#byteValue,#shortValue,#intValue,#longValue
-     *   当数值未超界时，可以得到正确的数值。当数值超界时，抛异常
-     * 3.BigDecimal转换成整数: BigDecimal#toBigInteger
-     *   得到精确的整数部分
-     *
-     * 4.浮点数强制转换成整数: 浮点数的存储 {@link com.fresh.common.flt.FloatTest}
-     *   取该浮点数在浮点数表中对应值的整数部分（不一定精确）
-     *
-     *
-     *number转换成BigInteger
-     * 1.BigDecimal转换成BigInteger: BigDecimal#toBigInteger
-     * 2.整数转换成BigInteger:   BigInteger完全能容纳
-     * 3.浮点数转换成BigInteger: 取该浮点数在浮点数表中对应值的整数部分（不一定精确），取出的整数部分如果超过long,将发生截断
-     *
-     *
-     *number转换成BigDecimal
-     * 1.BigInteger转换成BigDecimal，小数部分为0
-     * 2.整数转换成BigDecimal，小数部分为0
-     * 3.浮点数转换成BigDecimal，取该浮点数在浮点数表中对应值（不一定精确）构造BigDecimal，非精确
-     *
-     *
-     *number转换成浮点数
-     * 1.number是浮点数，大->小且超界undefined
-     * 2.整数转换成浮点数: Float#valueOf, Long的最大值不超过Float
-     * 3.BigInteger转换成浮点数: 超界undefined
-     * 4.BigDecimal转换成浮点数: 超界undefined
-     *
-     *
-     * @param number Number类型的源对象
-     * @param targetClazz 目标类型
-     * @return 转换后的值
+     * @param number the source number
+     * @param targetClazz the clazz of the target type
+     * @return target number
+     * @param <T> the target type
+     * @throws NullPointerException if the source number or the target clazz is null
+     * @throws IllegalArgumentException 如果目标类型是整数且原数值超过该整数类型的表示范围时抛出此异常
      */
+    @SuppressWarnings("unchecked")
     public static <T extends Number> T convertNumberToTargetClazz(Number number, Class<T> targetClazz) {
-        Assert.notNull(number, "参数 number[Number] 不能为空");
-        Assert.notNull(targetClazz, "参数 clazz[Class] 不能为空");
+        Assert.notNull(number, "参数 number 不能为空");
+        Assert.notNull(targetClazz, "参数 targetClazz 不能为空");
 
         if(targetClazz.isInstance(number)) {
             return (T) number;
         } else if(Byte.class == targetClazz || Byte.TYPE == targetClazz) {
             long l = resolveLongValue(number, targetClazz);
-            Assert.isTrue(l >= Byte.MIN_VALUE && l <= Byte.MAX_VALUE, "参数number["+number.getClass().getName()+"]的值{"+l+"}大于["+targetClazz.getName()+"]允许的最大值");
+            Assert.isTrue(l >= Byte.MIN_VALUE && l <= Byte.MAX_VALUE, "参数 number[" + number.getClass().getName() + "]的值{" + l + "}超过[" + targetClazz.getName() + "]的表示范围");
             return (T) Byte.valueOf(number.byteValue());
         } else if(Short.class == targetClazz || Short.TYPE == targetClazz) {
             long l = resolveLongValue(number, targetClazz);
-            Assert.isTrue(l >= Short.MIN_VALUE && l <= Short.MAX_VALUE, "参数number["+number.getClass().getName()+"]的值{"+l+"}大于["+targetClazz.getName()+"]允许的最大值");
+            Assert.isTrue(l >= Short.MIN_VALUE && l <= Short.MAX_VALUE, "参数 number[" + number.getClass().getName() + "]的值{" + l + "}超过[" + targetClazz.getName() + "]的表示范围");
             return (T) Short.valueOf(number.shortValue());
         } else if(Integer.class == targetClazz || Integer.TYPE == targetClazz) {
             long l = resolveLongValue(number, targetClazz);
-            Assert.isTrue(l >= Integer.MIN_VALUE && l <= Integer.MAX_VALUE, "参数number["+number.getClass().getName()+"]的值{"+l+"}大于["+targetClazz.getName()+"]允许的最大值");
+            Assert.isTrue(l >= Integer.MIN_VALUE && l <= Integer.MAX_VALUE, "参数 number[" + number.getClass().getName() + "]的值{" + l + "}超过[" + targetClazz.getName() + "]的表示范围");
             return (T) Integer.valueOf(number.intValue());
         } else if(Long.class == targetClazz || Long.TYPE == targetClazz) {
             long l = resolveLongValue(number, targetClazz);
             return (T) Long.valueOf(l);
         } else if(BigInteger.class == targetClazz) {
-            if(number instanceof  BigDecimal) {
+            if(number instanceof BigDecimal) {
                 return (T) ((BigDecimal) number).toBigInteger();
             } else {
                 return (T) BigInteger.valueOf(number.longValue());
@@ -191,11 +164,18 @@ public abstract class NumberUnitUtils {
         } else if(Double.class == targetClazz || Double.TYPE == targetClazz) {
             return (T) Double.valueOf(number.doubleValue());
         } else if(BigDecimal.class == targetClazz) {
-            return (T) new BigDecimal(number.toString());
+            if(number instanceof Float) {
+                return (T) new BigDecimal((Float) number);
+            } else if(number instanceof Double) {
+                return (T) new BigDecimal((Double) number);
+            } else if(number instanceof BigInteger) {
+                return (T) new BigDecimal((BigInteger) number);
+            } else {
+                return (T) new BigDecimal(number.longValue());
+            }
         } else {
-            throw new IllegalArgumentException("参数number["+number.getClass().getName()+"]不能转化为"+targetClazz.getName());
+            throw new IllegalStateException("不支持的类型[" + targetClazz.getName() + "]");
         }
-        //return null;
     }
 
     private static long resolveLongValue(Number number, Class<? extends Number> clazz) {
@@ -207,53 +187,48 @@ public abstract class NumberUnitUtils {
         }
         if (bigInteger != null && (bigInteger.compareTo(LONG_MIN) < 0 || bigInteger.compareTo(LONG_MAX) > 0)) {
             final String ngiStr = bigInteger.toString();
-            throw new IllegalArgumentException("参数number["+number.getClass().getName()+"]的数值{"+ngiStr+"}大于参数clazz["+clazz.getName()+"]允许的最大值");
+            throw new IllegalArgumentException("参数 number[" + number.getClass().getName() + "]的数值{" + ngiStr + "}超过参数 clazz[" + clazz.getName() + "]的表示范围");
         }
         return number.longValue();
     }
 
-    /*
-     *将字符串形式的数值转化为指定Number
-     *继承Number的子类型(标准库)有: Byte,Short,Integer,Long,BigInteger,Float,Double,BigDecimal
-     *16进制整数前缀: 0x,0X,#
+    /**
+     * 整数字面量（字符串形式）转化为整数数值类型。
      *
-     *转换成整数
-     * text表示的数值超界或者格式不能解析时，NumberFormatException
-     *转换成BigInteger，BigDecimal
-     * text格式不能解析时，NumberFormatException
-     *转换成浮点数
-     * text表示的数值超界时undefined, text格式不能解析时，NumberFormatException
-     *
-     * @param text
-     * @param clazz
-     * @param <T>
-     * @return
+     * @param text the specified text
+     * @param clazz 目标类型 Clazz
+     * @return 转化得到的数值
+     * @param <T> 目标类型
+     * @throws NullPointerException if the specified text or clazz is null
+     * @throws NumberFormatException if the text can not convert to a number
      */
+    @SuppressWarnings("unchecked")
     public static <T extends Number> T parseTextToTargetNumber(String text, Class<T> clazz) {
-        Assert.notNull(text, "text[String] 不能为空");
-        Assert.notNull(clazz, "参数 clazz[Class] 不能为空");
+        Assert.notNull(text, "参数 text 不能为空");
+        Assert.notNull(clazz, "参数 clazz 不能为空");
 
         String trimedText = StringUtils.trimAllWhitespace(text);
+
         if(Byte.class == clazz || Byte.TYPE == clazz) {
-            return (T) (StringUtils.isHexNumber(trimedText) ? Byte.decode(trimedText) : Byte.valueOf(trimedText));
+            return (T) Byte.decode(trimedText);
         } else if (Short.class == clazz || Short.TYPE == clazz) {
-            return (T) (StringUtils.isHexNumber(trimedText) ? Short.decode(trimedText) : Short.valueOf(trimedText));
+            return (T) Short.decode(trimedText);
         } else if (Integer.class == clazz || Integer.TYPE == clazz) {
-            return (T) (StringUtils.isHexNumber(trimedText) ? Integer.decode(trimedText) : Integer.valueOf(trimedText));
+            return (T) Integer.decode(trimedText);
         } else if (Long.class == clazz || Long.TYPE == clazz) {
-            return (T) (StringUtils.isHexNumber(trimedText) ? Long.decode(trimedText) : Long.valueOf(trimedText));
+            return (T) Long.decode(trimedText);
         } else if (BigInteger.class == clazz) {
-            return (T) (StringUtils.isHexNumber(trimedText) ? decodeBigInteger(trimedText) : new BigInteger(trimedText));
+            return (T) decodeBigInteger(trimedText);
         } else if (Float.class == clazz || Float.TYPE == clazz) {
             return (T) Float.valueOf(trimedText);
         } else if (Double.class == clazz || Double.TYPE == clazz) {
             return (T) Double.valueOf(trimedText);
         } else if (BigDecimal.class == clazz || Number.class == clazz) {
+            //todo. 当前实现，只处理十进制数，其他进制数抛 NumberFormatException
             return (T) new BigDecimal(trimedText);
         } else {
-            throw new IllegalArgumentException("参数text["+text+"]不能解析成"+clazz.getName());
+            throw new IllegalStateException("不支持的类型[" + clazz.getName() + "]");
         }
-        //return null;
     }
 
     private static BigInteger decodeBigInteger(String value) {
@@ -280,27 +255,30 @@ public abstract class NumberUnitUtils {
             index++;
             radix = 8;
         }
+
         BigInteger result = new BigInteger(value.substring(index), radix);
         return (negative ? result.negate() : result);
     }
 
-
+    /**
+     * 将对象转化为数值类型。
+     *
+     * @param o 对象
+     * @param clazz 数值类型 clazz
+     * @return 转化得到的数值
+     * @param <T> 数值类型
+     * @throws NullPointerException  if the specified object or clazz is null
+     * @throws IllegalArgumentException 如果目标类型是整数且原数值超过该整数类型的表示范围时抛出此异常
+     * @throws NumberFormatException 如果对象是字符串类型且该字符串不能表示为数值
+     */
     public static <T extends Number> T convertToNumber(Object o, Class<T> clazz) {
-
         if(o instanceof Number) {
-            return convertNumberToTargetClazz((Number)o, clazz);
+            return convertNumberToTargetClazz((Number) o, clazz);
+        } else if(o instanceof String) {
+            return parseTextToTargetNumber((String) o, clazz);
+        } else {
+            throw new IllegalStateException("不支持的类型[" + o.getClass() + "]");
         }
-        if(o instanceof String) {
-            return parseTextToTargetNumber((String)o, clazz);
-        }
-        return null;
-    }
-
-    public static <T extends Number> T convertToNumber(Object o, Class<T> clazz, T defaultValue) {
-        T value = convertToNumber(o, clazz);
-
-        if (value != null) return value;
-        else return defaultValue;
     }
 
     /*
@@ -311,7 +289,7 @@ public abstract class NumberUnitUtils {
      */
     @Deprecated
     public static Long convert2Long(Object o) {
-        if(o == null) throw new NumberFormatException("不能为null");
+        if(o == null) throw new NumberFormatException("不能为 null");
         if(o instanceof Number) return ((Number) o).longValue();
         return Long.parseLong(String.valueOf(o));
     }
@@ -346,15 +324,15 @@ public abstract class NumberUnitUtils {
      * @param n
      * @return
      */
-    public static final int closestPower(int n) {
+    public static int closestPower(int n) {
         if(n < 0) return 1;
         if(n >= MAXIMUM_CAPACITY) return MAXIMUM_CAPACITY;
-        int c = n - 1; //n已经是 2^m次方的情况
+        int c = n - 1; //n 已经是 2^m 次方的情况
         c |= c >>> 1;
         c |= c >>> 2;
         c |= c >>> 4;
         c |= c >>> 8;
-        c |= c >>> 16;  //最多移16位的原因，与MAXIMUM_CAPACITY最大值相关
+        c |= c >>> 16;  //最多移 16 位的原因，与 MAXIMUM_CAPACITY 最大值相关
         return (c < 0) ? 1 : (c >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : c + 1;
     }
 
