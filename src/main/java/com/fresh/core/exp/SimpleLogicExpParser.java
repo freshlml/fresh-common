f17a19017f85c63fd6ec51d75bb0d9df98c30a68
freshlml <1326191992@qq.com>
2026-08-11 22:30:42 +0800

    1

--- src/main/java/com/fresh/core/exp/SimpleLogicExpParser.java
+++ src/main/java/com/fresh/core/exp/SimpleLogicExpParser.java
@@ -84,4 +84,26 @@ public abstract class SimpleLogicExpParser {
     private enum Context {
         NONE, NOT, SUB
     }
+
+
+    public static void main(String[] argv) {
+        SimpleLogicExp exp = null;
+        //exp = SimpleLogicExp.of("p1 & p2 & p3");
+        exp = SimpleLogicExp.of("p1 & (p2| ( p3&p4) )");
+        //exp = SimpleLogicExp.of("p1 & !p2");
+        //exp = SimpleLogicExp.of("p1 & !(p2|p3)");
+        //exp = SimpleLogicExp.of("p1 & (!p2|p3)");
+        //exp = SimpleLogicExp.of("!p1");
+        //exp = SimpleLogicExp.of("p1 & p2 | p3");
+        System.out.println(exp.matches(SimpleLogicExpParser::expMatchers));
+    }
+
+    static boolean expMatchers(String exp) {
+        List<String> activeExps = new ArrayList<>();
+        activeExps.add("p1");
+        //activeExps.add("p2");
+        activeExps.add("p3");
+        activeExps.add("p4");
+        return activeExps.contains(exp);
+    }
 }
