206587ffebbcfc59fe9dd397a8d02a2a91e2078b
freshlml <1326191992@qq.com>
2026-08-11 22:21:41 +0800

    1

--- src/main/java/com/fresh/core/exp/SimpleLogicExp.java
+++ src/main/java/com/fresh/core/exp/SimpleLogicExp.java
@@ -4,11 +4,11 @@ import java.util.function.Predicate;
 
 /*
  * provide a simple logic expression: & | !
- * using "()" to contains a sub-expression:  (p1 &p2 & (p2-1 | p2-2)) | ! p3 | (p4 ) | !(p5 & p6)
+ * using "()" to contains a sub-expression:  (p1 & p2 & (p3 | p4)) | ! p5 | (p6 ) | !(p7 & p8)
  * eg: {expression="p1 & p2 & p3"
- *       return true only when predicate("p1")=true and  predicate("p2")=true and predicate("p3")=true
+ *       return true only when `predicate("p1")=true` and `predicate("p2")=true` and `predicate("p3")=true`
  *     }
- * if expression="p1 & p2 | p3", its meanless and were throw IllegalArgumentException
+ * if expression="p1 & p2 | p3", its meanness and were throw IllegalArgumentException
  * for this, we can using its in sub-expression,eg: {expression="(p1 & p2) | p3"
  *                                           the whole expression is "(p1 & p2) | p3"
  *                                           where using "()" contains sub-expression
